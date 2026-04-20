e8fe58a133bb1179792530ea6471181e495222c4
Sam Welborn <swelborn@slac.stanford.edu>
2026-04-20 11:38:11 -0400

    use settings

--- src/amsc_openmetadata_tiled_sync/main.py
+++ src/amsc_openmetadata_tiled_sync/main.py
@@ -1,15 +1,18 @@
 import json
-import os
 import traceback
 from functools import partial
 
 import httpx
 from tiled.client import from_uri
+from tiled.stream_messages import ChildCreated
+from tiled.structures.core import StructureFamily
+
+from .config import OpenMetadataSettings, Settings
 
 
-def build_body(update, tiled_uri):
+def build_body(update: ChildCreated, tiled_uri: str):
     metadata = update.metadata
-    if update.structure_family == "container":
+    if update.structure_family == StructureFamily.container:
         body = {
             "type": "artifactCollection",
             "name": update.key,
@@ -32,15 +35,19 @@ def build_body(update, tiled_uri):
     return body
 
 
-def upload(update, tiled_uri, client):
+def upload(
+    update: ChildCreated,
+    tiled_uri: str,
+    client: httpx.Client,
+    settings: OpenMetadataSettings,
+):
     try:
         body = build_body(update, tiled_uri)
         entity_type = body["type"]
-        catalog_name = os.environ["AMSC_OPENMETADATA_CATALOG_NAME"]
         response = client.post(
-            f"/catalog/{catalog_name}/{entity_type}",
+            f"/catalog/{settings.catalog_name}/{entity_type}",
             headers={
-                "Authorization": f"Bearer {os.environ['AMSC_OPENMETADATA_TOKEN']}",
+                "Authorization": f"Bearer {settings.token}",
                 "Accept": "application/json",
                 "Content-Type": "application/json",
             },
@@ -51,10 +58,21 @@ def upload(update, tiled_uri, client):
         traceback.print_exc()
 
 
-def listen(tiled_uri):
+def listen():
+    settings = Settings()
+    tiled_uri = settings.tiled.uri.unicode_string()
     tiled_client = from_uri(tiled_uri)
-    client = httpx.Client(base_url="https://api.american-science-cloud.org/api/current")
+
+    # omd client
+    omd_base_url = settings.amsc_openmetadata.base_url.unicode_string()
+    client = httpx.Client(base_url=omd_base_url)
+
     sub = tiled_client.subscribe()
-    callback = partial(upload, tiled_uri=tiled_uri, client=client)
+    callback = partial(
+        upload,
+        tiled_uri=tiled_uri,
+        client=client,
+        settings=settings.amsc_openmetadata,
+    )
     sub.child_created.add_callback(callback)
     sub.start()  # block
